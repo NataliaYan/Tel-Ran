00830c20ebda11edd271ff47278fb1dce07be15e
NataliaYan <125498119+NataliaYan@users.noreply.github.com>
2023-03-27 23:18:00 +0200

    HWLinux25.03.2023 NEWNEW
    1 Создайте скрипт sleeper.sh, который будет 10 раз с интервалом в 5 секунд писать дату в формате HH:MM:SS и количество процессов одним числом.
    2 Уменьшите или уберите временной интервал (который нам дает sleep), используя vi или nano, закомментировав строку или поменяв значение sleep
    3 С помощью скрипта запишите в файл информацию о процессоре. 
    4 С помощью скрипта запишите в файл информацию об операционной системе, но отфильтруйте информацию так, чтобы осталось только имя (NAME=Alpine Linux).
    5 Выполните прошлое действие, но так, чтобы слово NAME= не осталось, а было только имя в чистом виде (Alpine)
    6 С помощью скрипта создайте 50 файлов с расширением txt и именами от 50.txt до 100.txt
    Вот что вам понадобится для реализации всего этого: 
    Если удобно, то вот однострочник, создающий простейший скрипт, который надо будет редактировать: 
    echo -e #!/bin/bash\n date\n echo “it works!“ > /tmp/script.sh

--- HWLinux25.03.2023/sleeper.sh
+++ HWLinux25.03.2023/sleeper.sh
@@ -6,8 +6,8 @@
  sleep .5
  done
 lscpu > file.txt
-cat /etc/os-release | head -1 >> file.txt
-cat /etc/os-release | head -1 | cut -d' ' -f1 | cut -d'"' -f2 >> file.txt
+grep NAME /etc/os-release | head -1 >> file.txt
+grep NAME /etc/os-release | head -1 | cut -d' ' -f1 | cut -d'"' -f2 >> file.txt
 for run in {50..100}
 do
 touch $run.txt
